dcea24235632b80c97c4a7bd19f840f7266a70d1
PaulinaBdance <92453421+PaulinaBdance@users.noreply.github.com>
2022-07-05 18:44:42 +0200

    completed task

---
+++ js/script.js
@@ -0,0 +1,34 @@
+document,addEventListener('DOMContentLoaded', function() {
+
+    const nav = document.querySelector('.navbar')
+    const allNavItems = document.querySelectorAll('.nav-link')
+    const navList = document.querySelector('.navbar-collapse')
+
+    function addShadow() {
+
+        if (window.scrollY >= 300) {
+            nav.classList.add('shadow-bg')
+        } else {
+            nav.classList.remove('shadow-bg')
+        }
+    }
+
+    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+
+
+
+
+
+    window.addEventListener('scroll', addShadow)    
+})
+
+
+/*const hide = document.querySelector('navbar')
+ 
+function show() {
+    if (hide, 'click') {
+        nav.classList.add('show')
+    }else {
+        nav.classList.remove('show')
+    }
+}*/
